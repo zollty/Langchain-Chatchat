6f50b546089c9e64097e29b2eb25f25d190581ba
zollty <zollty@163.com>
2024-01-08 17:50:22 +0800

    update

--- webui_pages/dialogue/dialogue.py
+++ webui_pages/dialogue/dialogue.py
@@ -298,7 +298,7 @@ def dialogue_page(api: ApiRequest, is_lite: bool = False):
                     # call auto_summary
                     st.session_state["need_summary"] = True
                     tmp_file_name = st.session_state["file_chat_files"][0]
-                    info_placeholder.text(info_placeholder.value + "\n" + tmp_file_name)
+                    info_placeholder.text(info_placeholder.value() + "\n" + tmp_file_name)
 
         elif dialogue_mode == "搜索引擎问答":
             search_engine_list = api.list_search_engines()
