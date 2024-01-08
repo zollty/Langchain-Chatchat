b34ba6d78e2ed63758150837ef07b4a32f19e357
zollty <zollty@163.com>
2024-01-08 15:53:11 +0800

    update

--- server/chat/doc_summary.py
+++ server/chat/doc_summary.py
@@ -3,6 +3,7 @@ from fastapi.responses import StreamingResponse
 from fastapi.concurrency import run_in_threadpool
 from server.utils import wrap_done, get_ChatOpenAI
 from server.utils import BaseResponse, get_prompt_template
+from server.chat.utils import History
 from langchain.chains import LLMChain
 from langchain.callbacks import AsyncIteratorCallbackHandler
 from typing import AsyncIterable
