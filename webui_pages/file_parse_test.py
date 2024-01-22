04c4883abd418694ecbc57327654e0235199ba9a
zollty <zollty@163.com>
2024-01-22 19:54:07 +0800

    update

--- webui_pages/file_parse_test.py
+++ webui_pages/file_parse_test.py
@@ -50,10 +50,10 @@ def test_file_parse_page(api: ApiRequest, is_lite: bool = None):
             for d in docs:
                 dtext += [id["page_content"] for id in d]
             vak = "\n\n\n\n".join(dtext)
-            het = len(vak.split("\n")) + 4
+            het = len(vak.split("\n"))
             print(f"---------------------------------------het: {het}")
             st.divider()
-            doc_info = st.text_area("解析后的文档:", max_chars=None, key="doc_info", value=vak, height=het*22, help=None, on_change=None, args=None, kwargs=None)
+            doc_info = st.text_area("解析后的文档:", max_chars=None, key="doc_info", value=vak, height=het*24, help=None, on_change=None, args=None, kwargs=None)
         elif msg := check_error_msg(ret):
             st.toast(msg, icon="✖")
             vak = json.dumps(ret.get("data").get("failed_files"))
