76c9e6490b0266e2e7a7b1a5685c0237bd51513d
zollty <zollty@163.com>
2024-01-22 20:05:33 +0800

    update

--- webui_pages/file_parse_test.py
+++ webui_pages/file_parse_test.py
@@ -54,7 +54,7 @@ def test_file_parse_page(api: ApiRequest, is_lite: bool = None):
             print(f"---------------------------------------het: {het}")
             st.divider()
             st.text("解析后的文档:")
-            st.text(vak)
+            st.code(vak, language="None", line_numbers=True)
             # doc_info = st.text_area("解析后的文档:", max_chars=None, key="doc_info", value=vak, height=het*40, help=None, on_change=None, args=None, kwargs=None)
         elif msg := check_error_msg(ret):
             st.toast(msg, icon="✖")
