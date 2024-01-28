173ccd8ba35301a4755663684767083fa25f9d68
zollty <zollty@163.com>
2024-01-28 17:24:42 +0800

    update

--- webui_pages/ybychat_qa.py
+++ webui_pages/ybychat_qa.py
@@ -157,8 +157,8 @@ def yby_qa_page(api: ApiRequest, is_lite: bool = False):
                 chat_box.update_msg(text, element_index=0)
             chat_box.update_msg(text, element_index=0, streaming=False)
             if sd := d.get("source_documents", []):
-                sdc = [s["content"] for s in sd]
-                chat_box.update_msg("\n\n".join(sdc), element_index=1, streaming=False)
+                sdc = [s["file_name"]+"-----------------------\n\n"+s["content"] for s in sd]
+                chat_box.update_msg("\n\n\n\n".join(sdc), element_index=1, streaming=False)
 
 
 
