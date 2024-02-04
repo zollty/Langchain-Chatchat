e7609ac92e2c0e91565029b2ac4e64a244236643
zollty <zollty@163.com>
2024-02-04 23:19:42 +0800

    update

--- webui_pages/model_management.py
+++ webui_pages/model_management.py
@@ -20,8 +20,8 @@ def config_aggrid(
         use_checkbox: bool = False,
 ) -> GridOptionsBuilder:
     gb = GridOptionsBuilder.from_dataframe(df)
-    gb.configure_column("desc", width=240)
-    gb.configure_column("usage", width=140)
+    gb.configure_column("desc", width=240, editable=True)
+    gb.configure_column("usage", width=140, editable=True)
     for (col, header), kw in columns.items():
         gb.configure_column(col, header, wrapHeaderText=True, **kw)
     gb.configure_selection(
