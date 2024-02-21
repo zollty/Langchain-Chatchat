748eaa4e6ebccb912d46fa7ddc55b595ae2318d7
zollty <zollty@163.com>
2024-02-21 16:36:03 +0800

    update

--- webui_pages/skills/skill_calculate.py
+++ webui_pages/skills/skill_calculate.py
@@ -1,8 +1,20 @@
 import streamlit as st
 from webui_pages.utils import *
 from server.agent.tools.calculate import calculate
+from configs import LLM_MODELS, TEMPERATURE, Agent_MODEL
+from server.utils import get_ChatOpenAI
+from server.agent import model_container
 
 def skill_calculate_page(api: ApiRequest = None, is_lite: bool = None):
+
+    if Agent_MODEL:
+        ## 如果有指定使用Agent模型来完成任务
+        model_name=Agent_MODEL
+    else:
+        model_name=LLM_MODELS[0]
+    
+    model_container.MODEL = get_ChatOpenAI(model_name=model_name, temperature=TEMPERATURE)
+
     st.subheader("数学计算(numexpr)工具")
     st.markdown("<h5>使用说明：</h5>\n\n <sub>1、自定义Agent问答：为保证问答质量，需要手动切换成Qwen-14B模型</sub>\n\n <sub>2、知识库、园博园、搜索引擎问答：为保证问答质量，需要手动切换成chatglm3-6B-32k模型</sub> \n\n", unsafe_allow_html=True)
     st.text("↓↓↓↓↓")
