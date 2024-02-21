060dd1c11b47f2f74e8dde10b6dd41cc0a4ed830
zollty <zollty@163.com>
2024-02-21 16:18:45 +0800

    update

--- webui_pages/skills/skill_shell.py
+++ webui_pages/skills/skill_shell.py
@@ -7,7 +7,7 @@ def skill_shell_page(api: ApiRequest = None, is_lite: bool = None):
     st.markdown("<h5>使用说明：</h5>\n\n <sub>1、自定义Agent问答：为保证问答质量，需要手动切换成Qwen-14B模型</sub>\n\n <sub>2、知识库、园博园、搜索引擎问答：为保证问答质量，需要手动切换成chatglm3-6B-32k模型</sub> \n\n", unsafe_allow_html=True)
     st.text("↓↓↓↓↓")
 
-    args = st.text_input(label="执行的命令", value=""， key="args")
+    args = st.text_input(label="执行的命令", value="", key="args")
     if st.button(
                 "运行",
                 use_container_width=False,
