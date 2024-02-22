02a9be33e70638c902235d4c4afa5793d80b71c1
zollty <zollty@163.com>
2024-02-22 11:20:03 +0800

    update

--- webui_pages/skills/skill_search_duckduckgo.py
+++ webui_pages/skills/skill_search_duckduckgo.py
@@ -3,6 +3,17 @@ from webui_pages.utils import *
 from server.chat.search_engine_chat import duckduckgo_search
 
 def skill_search_duckduckgo_page(api: ApiRequest = None, is_lite: bool = None):
+    st.markdown(
+        """
+    <style>
+        code {
+            white-space : pre-wrap !important;
+            word-break: break-word;
+        }
+    </style>
+    """,
+        unsafe_allow_html=True,
+    )
     st.subheader("免费互联网搜索引擎工具")
     st.markdown("<h5>使用说明：</h5>\n\n 1、输入搜索关键字 \n\n", unsafe_allow_html=True)
     st.text("↓↓↓↓↓")
@@ -15,7 +26,7 @@ def skill_search_duckduckgo_page(api: ApiRequest = None, is_lite: bool = None):
         ):
         print("-------------------------")
         ret = duckduckgo_search(args)
-        st.code(ret, language="None", line_numbers=True)
+        st.code(ret, language="json", line_numbers=False)
 
     st.text("↑↑↑↑↑↑")
     st.markdown("<h4>注意事项：</h4>\n\n1、请勿频繁使用，免费API每日限制100次！！", unsafe_allow_html=True)
