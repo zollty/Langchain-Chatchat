69813374210debf961ef183bc27cbb7bb7ffdbaf
zollty <zollty@163.com>
2024-02-22 10:32:42 +0800

    update

--- webui_pages/skills/skill_calculate.py
+++ webui_pages/skills/skill_calculate.py
@@ -19,7 +19,8 @@ def skill_calculate_page(api: ApiRequest = None, is_lite: bool = None):
     st.markdown("<h5>使用说明：</h5>\n\n <sub>1、自定义Agent问答：为保证问答质量，需要手动切换成Qwen-14B模型</sub>\n\n <sub>2、知识库、园博园、搜索引擎问答：为保证问答质量，需要手动切换成chatglm3-6B-32k模型</sub> \n\n", unsafe_allow_html=True)
     st.text("↓↓↓↓↓")
 
-    args = st.text_input(label="执行的命令", value="", key="args")
+    args = st.text_input(label="执行的命令：", value="sin(45)", key="args")
+    st.markdown("用法示例：1.234^5、10*23+12")
     if st.button(
                 "运行",
                 disabled=(args == None),
--- webui_pages/skills/skill_wolfram.py
+++ webui_pages/skills/skill_wolfram.py
@@ -6,8 +6,8 @@ def skill_wolfram_page(api: ApiRequest = None, is_lite: bool = None):
     st.subheader("在线科学计算（wolfram）工具")
     st.markdown("<h5>使用说明：</h5>\n\n <sub>1、自定义Agent问答：为保证问答质量，需要手动切换成Qwen-14B模型</sub>\n\n <sub>2、知识库、园博园、搜索引擎问答：为保证问答质量，需要手动切换成chatglm3-6B-32k模型</sub> \n\n", unsafe_allow_html=True)
     st.text("↓↓↓↓↓")
-
-    args = st.text_input(label="执行的命令", value="", key="args")
+    args = st.text_input(label="执行的命令：（高级函数、方程式）", value="10th derivative of 1/(1+x)", key="args")
+    st.markdown("用法示例：https://wenku.so.com/d/4e0aeccf51fd04a19dda9784c720ebc5")
     if st.button(
                 "运行",
                 disabled=(args == None),
