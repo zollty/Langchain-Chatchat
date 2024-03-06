a6d283e5b5b16a175938ddb5d5dd6acf9fcc022e
zollty <zollty@163.com>
2024-03-07 01:36:59 +0800

    update

--- server/chat/chat.py
+++ server/chat/chat.py
@@ -1,6 +1,5 @@
 from fastapi import Body
-#from sse_starlette.sse import EventSourceResponse
-from fastapi.responses import StreamingResponse
+from sse_starlette.sse import EventSourceResponse
 from configs import LLM_MODELS, TEMPERATURE
 from server.utils import wrap_done, get_ChatOpenAI
 from langchain.chains import LLMChain
@@ -108,5 +107,4 @@ async def chat(query: str = Body(..., description="用户输入", examples=["恼
 
         await task
 
-    #return EventSourceResponse(chat_iterator())
-    return StreamingResponse(chat_iterator(), media_type="text/event-stream")
+    return EventSourceResponse(chat_iterator())
