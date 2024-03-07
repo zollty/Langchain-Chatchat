56f122b549f2efd59727cc8d7eef1c2d3ad58e23
zollty <zollty@163.com>
2024-03-07 17:33:09 +0800

    update

--- server/knowledge_base/kb_doc_api.py
+++ server/knowledge_base/kb_doc_api.py
@@ -11,6 +11,7 @@ from server.knowledge_base.utils import (validate_kb_name, list_files_from_folde
 from fastapi.responses import FileResponse
 from sse_starlette import EventSourceResponse
 import json
+from pydantic import Json
 from server.knowledge_base.kb_service.base import KBServiceFactory
 from server.db.repository.knowledge_file_repository import get_file_detail
 from langchain.docstore.document import Document
@@ -151,7 +152,7 @@ def upload_docs(
         chunk_size: int = Form(CHUNK_SIZE, description="知识库中单段文本最大长度"),
         chunk_overlap: int = Form(OVERLAP_SIZE, description="知识库中相邻文本重合长度"),
         zh_title_enhance: bool = Form(ZH_TITLE_ENHANCE, description="是否开启中文标题加强"),
-        docs: Dict = Form({}, description="自定义的docs，需要转为json字符串"),
+        docs: Json = Form({}, description="自定义的docs，需要转为json字符串"),
         not_refresh_vs_cache: bool = Form(False, description="暂不保存向量库（用于FAISS）"),
 ) -> BaseResponse:
     """
@@ -252,7 +253,7 @@ def update_docs(
         chunk_overlap: int = Body(OVERLAP_SIZE, description="知识库中相邻文本重合长度"),
         zh_title_enhance: bool = Body(ZH_TITLE_ENHANCE, description="是否开启中文标题加强"),
         override_custom_docs: bool = Body(False, description="是否覆盖之前自定义的docs"),
-        docs: Dict = Body({}, description="自定义的docs，需要转为json字符串"),
+        docs: Json = Body({}, description="自定义的docs，需要转为json字符串"),
         not_refresh_vs_cache: bool = Body(False, description="暂不保存向量库（用于FAISS）"),
 ) -> BaseResponse:
     """
