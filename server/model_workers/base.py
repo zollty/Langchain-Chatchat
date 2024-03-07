1b09b5c639822532f6459d63255b20c12c062318
zollty <zollty@163.com>
2024-03-07 17:55:53 +0800

    update

--- server/model_workers/base.py
+++ server/model_workers/base.py
@@ -6,8 +6,8 @@ from fastchat.serve.base_model_worker import BaseModelWorker
 import uuid
 import json
 import sys
-from pydantic.v1 import BaseModel, root_validator
-#from langchain.pydantic_v1 import Extra, root_validator
+from pydantic.v1 import BaseModel
+from langchain.pydantic_v1 import Extra, root_validator
 import fastchat
 import asyncio
 from server.utils import get_model_worker_config
