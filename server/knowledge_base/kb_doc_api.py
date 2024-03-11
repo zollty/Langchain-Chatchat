ccb211b265e40f7ca86fb97a3ea87c99ce1e2ff4
zollty <zollty@163.com>
2024-03-11 18:02:53 +0800

    update

--- server/knowledge_base/kb_doc_api.py
+++ server/knowledge_base/kb_doc_api.py
@@ -40,11 +40,11 @@ def search_docs(
         elif file_name or metadata:
             data = kb.list_docs(file_name=file_name, metadata=metadata)
     
-    data = [
-        {"seq": i + 1, "id": x.id, "page_content": x.page_content, "source": x.metadata.get("source"),
-         "type": x.type,
-         "metadata": json.dumps(x.metadata, ensure_ascii=False),
-         } for i, x in enumerate(data)]
+    # data = [
+    #     {"seq": i + 1, "id": x.id, "page_content": x.page_content, "source": x.metadata.get("source"),
+    #      "type": x.type,
+    #      "metadata": json.dumps(x.metadata, ensure_ascii=False),
+    #      } for i, x in enumerate(data)]
     # data = [
     #     {"id": 1, "page_content": "xxxxxxxxxxxxxxxx", "source": "/ddd/ddd/aaa",
     #         "type": "txt",
