232cbbe3a649c73a88e9ab3e8e45ed77fa810f49
zollty <zollty@163.com>
2024-03-11 17:58:45 +0800

    update

--- server/knowledge_base/model/kb_document_model.py
+++ server/knowledge_base/model/kb_document_model.py
@@ -1,6 +1,6 @@
 
 from langchain.docstore.document import Document
-from langchain_core.pydantic_v1 import Field
+from langchain_core.pydantic_v1 import Field, root_validator
 
 class DocumentWithVSId(Document):
     """
@@ -8,3 +8,7 @@ class DocumentWithVSId(Document):
     """
     id: str = Field(None, description="API data")
     score: float = Field(3.0, description="API data")
+    
+    @root_validator(pre=True)
+    def validate_config(cls, v: Dict) -> Dict:
+        pass
