11442b3fdac7d897781d1ef1415f81e38d677e07
zollty <zollty@163.com>
2024-03-11 17:52:54 +0800

    update

--- server/knowledge_base/model/kb_document_model.py
+++ server/knowledge_base/model/kb_document_model.py
@@ -1,10 +1,10 @@
 
 from langchain.docstore.document import Document
-
+from langchain_core.pydantic_v1 import Field
 
 class DocumentWithVSId(Document):
     """
     矢量化后的文档
     """
-    id: str = None
-    score: float = 3.0
+    id: str = Field(None, description="API data")
+    score: float = Field(3.0, description="API data")
