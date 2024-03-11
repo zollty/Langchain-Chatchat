ad32c28f088544a93ff1dc9846dc0067bc9d395c
zollty <zollty@163.com>
2024-03-11 18:22:05 +0800

    update

--- server/knowledge_base/kb_doc_api.py
+++ server/knowledge_base/kb_doc_api.py
@@ -43,15 +43,8 @@ def search_docs(
     data = [
         {"seq": i + 1, "id": x.id, "page_content": x.page_content, "source": x.metadata.get("source"),
          "type": x.type,
-         "metadata": x.metadata#json.dumps(x.metadata, ensure_ascii=False),
+         "metadata": x.metadata #json.dumps(x.metadata, ensure_ascii=False),
          } for i, x in enumerate(data)]
-    # data = [
-    #     {"id": 1, "page_content": "xxxxxxxxxxxxxxxx", "source": "/ddd/ddd/aaa",
-    #         "type": "txt",
-    #         "metadata": {},
-    #         } for i, x in enumerate(data)]
-    # data = [{"id": 1, "page_content": "xxxxxxxxxxxxxxxx"}]
-    print(f"---------------------{data}")
     return data
 
 
--- webui_pages/knowledge_base/knowledge_base.py
+++ webui_pages/knowledge_base/knowledge_base.py
@@ -336,9 +336,9 @@ def knowledge_base_page(api: ApiRequest, is_lite: bool = None):
             file_name = selected_rows[0]["file_name"]
             docs = api.search_kb_docs(knowledge_base_name=selected_kb, file_name=file_name)
             #docs = docs.get("data", {})
-            for i, x in enumerate(docs):
-                print(x)
-            print("----------------------98938")
+            # for i, x in enumerate(docs):
+            #     print(x)
+            # print("----------------------98938")
             data = [
                 {"seq": i + 1, "id": x["id"], "page_content": x["page_content"], "source": x["metadata"].get("source"),
                  "type": x["type"],
