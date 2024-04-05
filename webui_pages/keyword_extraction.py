33e947db2e74e219ff0cfe8dbe2a9af3b2c0f9ec
zollty <zollty@163.com>
2024-04-05 20:17:42 +0800

    update

--- webui_pages/keyword_extraction.py
+++ webui_pages/keyword_extraction.py
@@ -17,7 +17,7 @@ def remote_api(
         address = new_api_url
         with get_httpx_client() as client:
             r = client.post(address + "/spchat/keyword_extraction",
-                json={"sentence": sentence},
+                data={"sentence": sentence},
             )
             return r.json()
     except Exception as e:
