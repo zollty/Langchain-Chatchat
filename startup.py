11fdedddd320c7421e8b846ecefb29b61d7a6a08
zollty <zollty@163.com>
2024-10-20 23:24:09 +0800

    update

--- startup.py
+++ startup.py
@@ -502,7 +502,7 @@ def run_webui(started_event: mp.Event = None, run_mode: str = None):
     cmd = ["streamlit", "run", "webui.py",
            "--server.address", host,
            "--server.port", str(port),
-           "--theme.base", "dark",
+           # "--theme.base", "dark",
            "--theme.primaryColor", "#165dff",
            "--theme.secondaryBackgroundColor", "#f5f5f5",
            "--theme.textColor", "#000000",
